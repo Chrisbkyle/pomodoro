d8489482c5d6fbd99c87778bb4ef09dd63404fd1
Chris <christopher.b.kyle@gmail.com>
2024-05-14 22:59:26 +1200

    password hashing backend done

--- server/schema/userSchema.js
+++ server/schema/userSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const {Schema} = mongoose
+const bcrypt = require('bcrypt')
 const { isEmail, isStrongPassword } = require('validator')
 
 const userSchema = new Schema({
@@ -34,6 +35,21 @@ const userSchema = new Schema({
         type: Array,
         required: false
     }
+})
+
+//fire function after uesr saved to db
+userSchema.post('save', function(doc, next) {
+        console.log('new user was created and saved', doc)
+        next();
+    }
+)
+
+//fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
+    console.log('user about to be created and saved', this)
+    next()
 })
 
 module.exports = mongoose.model('user', userSchema)
